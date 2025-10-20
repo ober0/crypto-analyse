98fa86bddd616775b182a95d47c7598fd78e7660
Ruslan <136484210+ober0@users.noreply.github.com>
2025-10-20 17:20:44 +0300

    fix

--- src/modules/app/app.module.ts
+++ src/modules/app/app.module.ts
@@ -5,9 +5,8 @@ import { ConfigModule } from "@nestjs/config";
 import { AuthModule } from "../auth/auth.module";
 import { UserModule } from "../user/user.module";
 import { CryptModule } from "../crypt/crypt.module";
-import { RedisModule } from "../redis/redis.module";
 
 @Module({
-    imports: [PrismaModule, ConfigModule.forRoot({ isGlobal: true }), UserModule, AuthModule, CryptModule, RedisModule]
+    imports: [PrismaModule, ConfigModule.forRoot({ isGlobal: true }), UserModule, AuthModule, CryptModule]
 })
 export class AppModule {}
